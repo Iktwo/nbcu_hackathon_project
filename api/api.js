9edca511710c1cd7bee7a7f03eb05d1298ebd6ba
Niraj Desai <niraj.desai@daimler.com>
2016-03-05 12:46:22 -0800

    Updated API

--- api/api.js
+++ api/api.js
@@ -3,17 +3,20 @@ var ParseServer = require('parse-server').ParseServer;
 
 var app = express();
 var api = new ParseServer({
-      databaseURI: 'mongodb://nbcuhack',
-      cloud: './cloud/main.js',
+	databaseURI: 'mongodb://127.0.0.1:27017',
       appId: 'nbcuhackAppId',
       fileKey: 'nbcuhackFileKey',
-      masterKey: 'nbcuhackSecretKey'//,
-      //push: { ... }, // See the Push wiki page
-      //filesAdapter: ...,
+      masterKey: 'nbcuhackSecretKey',
+      serverURL: 'http://localhost:1337'
 });
 
 // Serve the Parse API at /parse URL prefix
 app.use('/parse', api);
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function(req, res) {
+  res.status(200).send('nbcuhack');
+});
 
 var port = 1337;
 app.listen(port, function() {
